b2334ae205b5500e5d82b01eabe84d9f7cca6d54
azizkunbaru-blip <azizkunbaru@gmail.com>
2026-01-08 22:29:32 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,94 @@
+// data.js
+window.DEBT_DATA = [
+  {
+    name: "MAS PIENG",
+    nyicil: 300000,
+    sisa: 372000,
+    items: [
+      { id: "pieng-kredivo-1", bank: "Kredivo", amount: 112000, due: "2026-01-24" },
+      { id: "pieng-kredivo-2", bank: "Kredivo", amount: 112000, due: "2026-01-25" },
+      { id: "pieng-kredivo-3", bank: "Kredivo", amount: 112000, due: "2026-01-26" },
+      { id: "pieng-allobank-1", bank: "Allobank", amount: 112000, due: "2026-01-31" },
+      { id: "pieng-allobank-2", bank: "Allobank", amount: 112000, due: "2026-01-31" },
+    ],
+  },
+  {
+    name: "MAS IS",
+    nyicil: 0,
+    sisa: 701000,
+    items: [
+      { id: "is-jago-1", bank: "Bank Jago", amount: 359000, due: "2026-01-25" },
+      { id: "is-superbank-1", bank: "Superbank", amount: 230000, due: "2026-02-01" },
+      { id: "is-allobank-1", bank: "Allobank", amount: 111100, due: "2026-02-01" },
+    ],
+  },
+  {
+    name: "MURIA",
+    nyicil: 0,
+    sisa: 112000,
+    items: [
+      { id: "muria-allobank-1", bank: "Allobank", amount: 112000, due: "2026-02-01" },
+    ],
+  },
+  {
+    name: "MAHMUD",
+    nyicil: 0,
+    sisa: 548000,
+    items: [
+      { id: "mahmud-kredivo-1", bank: "Kredivo", amount: 218000, due: "2026-01-24" },
+      { id: "mahmud-kredivo-2", bank: "Kredivo", amount: 112000, due: "2026-01-25" },
+      { id: "mahmud-kredivo-3", bank: "Kredivo", amount: 218000, due: "2026-01-26" },
+    ],
+  },
+  {
+    name: "MAHES",
+    nyicil: 0,
+    sisa: 168000,
+    items: [
+      { id: "mahes-kredivo-1", bank: "Kredivo", amount: 112000, due: "2026-01-22" },
+      { id: "mahes-allobank-1", bank: "Allobank", amount: 56100, due: "2026-02-01" },
+    ],
+  },
+  {
+    name: "JALI",
+    nyicil: 0,
+    sisa: 872000,
+    items: [
+      { id: "jali-jago-1", bank: "Bank Jago", amount: 760000, due: "2025-12-25" },
+      { id: "jali-allobank-1", bank: "Allobank", amount: 111100, due: "2026-02-01" },
+    ],
+  },
+  {
+    name: "SIDIK",
+    nyicil: 0,
+    sisa: 340000,
+    items: [
+      { id: "sidik-superbank-1", bank: "Superbank", amount: 340000, due: "2026-02-01" },
+    ],
+  },
+  {
+    name: "HADI",
+    nyicil: 0,
+    sisa: 212000,
+    items: [
+      { id: "hadi-superbank-1", bank: "Superbank", amount: 212000, due: "2026-02-01" },
+    ],
+  },
+  {
+    name: "BAWOR",
+    nyicil: 50000,
+    sisa: 347000,
+    items: [
+      { id: "bawor-superbank-1", bank: "Superbank", amount: 341000, due: "2026-02-01" },
+      { id: "bawor-allobank-1", bank: "Allobank", amount: 56100, due: "2026-02-01" },
+    ],
+  },
+  {
+    name: "KAPTEN",
+    nyicil: 0,
+    sisa: 355000,
+    items: [
+      { id: "kapten-jago-1", bank: "Bank Jago", amount: 355000, due: "2026-01-25" },
+    ],
+  },
+];
