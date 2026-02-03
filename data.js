a8a39754f1ee1eba16ca4fc76a840cf48b0a7a61
azizkunbaru-blip <azizkunbaru@gmail.com>
2026-02-03 15:41:02 +0700

    Update data.js

--- data.js
+++ data.js
@@ -36,6 +36,7 @@ window.DEBT_DATA = [
       { id: "mahmud-kredivo-4", bank: "Kredivo", amount: 112000, due: "2026-02-18" },
       { id: "mahmud-kredivo-5", bank: "Kredivo", amount: 273000, due: "2026-02-24" },
       { id: "mahmud-allobank-1", bank: "Allobank", amount: 112000, due: "2026-03-01" },
+      { id: "mahmud-allobank-2", bank: "Allobank", amount: 112000, due: "2026-03-01" },
     ],
   },
   {
